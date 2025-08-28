15a55e3de567f7db9306d1d04a7acbddf6b16c85
Arfan <pathanarfan8@gmail.com>
2025-08-28 22:16:25 +0530

    homepage-auth

---
+++ app/routes/home.tsx
@@ -0,0 +1,48 @@
+import Navbar from "~/components/Navbar";
+import type { Route } from "./+types/home";
+import { resumes } from "constants/index";
+import Resumecard from "~/components/Resumecard";
+import { usePuterStore } from "~/lib/puter";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Hirelytics" },
+    {
+      name: "description",
+      content: "Where resumes meet data-driven decisions.",
+    },
+  ];
+}
+
+export default function Home() {
+  const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+  const next = location.search.split("next=")[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate("/auth?next=/");
+  }, [auth.isAuthenticated]);
+
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <Resumecard key={resume.id} resume={resume}></Resumecard>
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
+}
